170fcfcf80677bc01e7ccc55506c8bc29dbd0791
Ridwan Abiola <ricklick262@gmail.com>
2018-05-21 12:37:43 +0100

    view all meetups and specific meetup

--- src/components/Meetup/Meetups.vue
+++ src/components/Meetup/Meetups.vue
@@ -1,5 +1,34 @@
 <template>
-  <div>
-      <p>The Meetups Page</p>
-  </div>
+    <v-container>
+        <v-layout wrap row>
+            <v-flex xs12>
+                <v-card class="info">
+                    <v-container fluid="">
+                        <v-layout row>
+                            <v-flex xs5 sm4 md3>
+                                <v-card-media
+                                src="https://www.maxpixel.net/static/photo/1x/Islam-National-Museum-Saudi-Arabia-Riad-Arabia-2376472.jpg"
+                                height="130px">
+                                </v-card-media>
+                            </v-flex>
+                            <v-flex xs7 sm8 md9>
+                                <v-card-title primary-title>
+                                    <div>
+                                        <div class="headline white--text">My Meetup</div>
+                                        <div>22nd May 2018</div>
+                                    </div>
+                                </v-card-title>
+                                <v-actions>
+                                    <v-btn flat to="/meetups/1">
+                                        <v-icon left light>arrow_forward</v-icon>
+                                        View Meetup
+                                    </v-btn>
+                                </v-actions>
+                            </v-flex>
+                        </v-layout>
+                    </v-container>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
 </template>
